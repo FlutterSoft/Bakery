0e477bbc9c54662f538fbc68450cef6de39aa074
Sam Brown <jumpbro@hotmail.com>
2022-05-26 18:11:47 +1000

    Fixing mobile issues
    menu not closing on link click

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const mobileNav = document.querySelector(".mobile-nav")
 const nav = document.querySelector(".main-nav")
 hamburger.addEventListener('click', toggleMenu)
 
+const mobileLinks = document.querySelectorAll(".nav-link")
+mobileLinks.forEach( link => link.addEventListener('click', toggleMenu))
 
 function toggleMenu(){
     nav.classList.toggle('blur')
