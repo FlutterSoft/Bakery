c690041776330d3cbf186c47f6b1adf2965f6ff2
Sam Brown <jumpbro@hotmail.com>
2022-05-28 15:38:28 +1000

    Completed, added readme and comments

---
+++ README.md
@@ -0,0 +1,34 @@
+# Drone.
+Website HTML/CSS/JS practise. Written from scratch. Based on a dribbble design. Fully responsive.
+
+**Link to project:** https://fakebakery.netlify.app/
+
+![screenshot](https://i.imgur.com/9GIwQk7.png)
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript
+
+This is a website made with HTML, CSS and JS. The CSS uses colour and font family variables, it includes media queries and is responsive. 
+The mobile navigation includes a transitional blur to the background for easier reading of the navigation on mobile. Hovering over nav links provides an underline animation. 
+
+## Optimizations
+
+Could use more helper classes and make components more reusable. 
+
+## Lessons Learned:
+
+Using helper classes is an easier way of adding the same styling to many elements. Make sections reusable for easier reuse throughout website. 
+
+## Examples:
+Some of my other work:
+
+**Drone Website:** https://github.com/FlutterSoft/Drone_Website
+
+**Magic Forest:** https://github.com/FlutterSoft/MagicForest
+
+**Hackerz:** https://github.com/FlutterSoft/hackerz
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
+// DOM Elements
 const hamburger = document.querySelector(".hamburger")
 const mobileNav = document.querySelector(".mobile-nav")
 const nav = document.querySelector(".main-nav")
+const mobileLinks = document.querySelectorAll(".nav-link")
+
+// Add toggleMenu function to the hamburger icon click
 hamburger.addEventListener('click', toggleMenu)
 
-const mobileLinks = document.querySelectorAll(".nav-link")
+// Add toggleMenu function to the mobile navigation clicks in order to close the nav when a nav item is clicked
 mobileLinks.forEach( link => link.addEventListener('click', toggleMenu))
 
 function toggleMenu(){
-    nav.classList.toggle('blur')
-    mobileNav.classList.toggle('blur')
-    mobileNav.classList.toggle('hidden')
-}
+    nav.classList.toggle('blur') // toggles background blur to the nav section
+    mobileNav.classList.toggle('blur') // toggles background blur to the mobile nav section
+    mobileNav.classList.toggle('hidden') // hides/shows the mobile nav section
 
-//add .blur to nav on click
-// toggle .hidden on mobile-nav on click
+    // Changes hamburger icon when menu is open/closed on mobile
+    if (hamburger.innerText == 'list'){
+        hamburger.innerText = 'menu_open'   
+        console.log('change to menu_open') 
+    }
+    else {
+        hamburger.innerText = 'list'
+        console.log('changed to list')
+    }
+
+}
